6eeda7c1612f3954cc740b2988999c12cf74a3d3
MayamaTakeshi <mayamatakeshi@gmail.com>
2020-08-26 16:54:34 +0900

    Preserving work so far

--- check_rtp_stream.py
+++ check_rtp_stream.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+import pypacker
 from pypacker import ppcap
 from pypacker.layer12 import ethernet
+from pypacker.layer12 import linuxcc
 from pypacker.layer3 import ip
 from pypacker.layer4 import udp
 from pypacker.layer567 import rtp
@@ -25,21 +27,38 @@ dst_port = int(dst_port)
 #print(pcap_file)
 
 preader = ppcap.Reader(filename=pcap_file)
+#print(dir(preader))
+
+ 
+def handle_packet(o, src_ip, src_port, dst_ip, dst_port):
+    if not (o[ip.IP].src_s == src_ip and o[udp.UDP].sport == src_port and o[ip.IP].dst_s == dst_ip and o[udp.UDP].dport == dst_port):
+        return
+
+    r = rtp.RTP(o[udp.UDP].body_bytes)
+
+    print("%d: pt=%s ts=%s seqnum=%s" % (
+        ts, 
+        r.pt,
+        r.ts,
+        r.seq
+    ))
+    sys.stdout.write("payload: ")
+    for b in r.body_bytes:
+        sys.stdout.write(hex(b) + " ")
+    print("")
+
 
 for ts, buf in preader:
     eth = ethernet.Ethernet(buf)
-
     if eth[ethernet.Ethernet, ip.IP, udp.UDP] is not None:
-        if eth[ip.IP].src_s == src_ip and eth[udp.UDP].sport == src_port and eth[ip.IP].dst_s == dst_ip and eth[udp.UDP].dport == dst_port:
-            r = rtp.RTP(eth[udp.UDP].body_bytes)
-            print("%d: pt=%s ts=%s seqnum=%s" % (
-                ts, 
-                r.pt,
-                r.ts,
-                r.seq
-            ))
-            sys.stdout.write("payload: ")
-            for b in r.body_bytes:
-                sys.stdout.write(hex(b) + " ")
-            print("")
+        #print("found eth")
+        handle_packet(eth, src_ip, src_port, dst_ip, dst_port)
+        continue
+
+    lcc = linuxcc.LinuxCC(buf)
+    if lcc[linuxcc.LinuxCC, ip.IP, udp.UDP] is not None:
+        #print("found lcc")
+        handle_packet(lcc, src_ip, src_port, dst_ip, dst_port)
+        continue
+
 
